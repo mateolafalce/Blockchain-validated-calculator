ccc9e7d779cad8574e9c2f28934fbc18e18aefb0
Mateo Lafalce <mateolafalce3@gmail.com>
2022-07-16 20:32:09 -0300

    16/7/2022

--- programs/calculator/src/lib.rs
+++ programs/calculator/src/lib.rs
@@ -1,36 +1,54 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("8UmDMaghFgLMnQXZXYnNJb2Uxpk1NUHMBo5ZbkeFYmwA");
 
 #[program]
 pub mod calculator {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        pub fn add(a: i64, b: i64) {
-            let c: i64 = a + b;
+    pub fn initialize(
+        ctx: Context<Initialize>, 
+        autoridad: Pubkey
+    ) -> Result<()> {
+
+        let number = &mut ctx.accounts.datastore;
+
+        number.autoridad = autoridad;
+
+        number.add1 = 23;
+        number.add2 = 34;
+        number.division1 = 34;
+        number.division2 = 98;
+
+        pub fn abc(num1: u64, num2: u64) {
+            let c: u64 = num1 + num2;
             msg!("The result of the sum is {}", c);
         }
-        pub fn subtraction(a: i64, b: i64){
-            let c: i64 = a - b;
-            msg!("The result of the subtraction is {}", c);  
-        }
-        pub fn multiplication(a: i64, b: i64){
-            let c: i64 = a * b;
-            msg!("The result of the multiplication is {}", c);  
-        }
-        pub fn division(a: i64, b: i64){
-            let c: i64 = a / b;
+        pub fn def(num1: u64, num2: u64){
+            let c: u64 = num1 / num2;
             msg!("The result of the division is {}", c);  
         }
 
-        add(2, 56);
-        subtraction(45, 6);
-        multiplication(23, 7);
-        division(56, 2);
+        abc(number.add1, number.add2);
+        def(number.division1, number.division2);
     Ok(())
 }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 30)]
+    pub datastore: Account<'info, DataStore>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct DataStore {
+    add1: u64,
+    add2: u64,
+    division1: u64,
+    division2: u64,
+    autoridad: Pubkey
+}
--- tests/calculator.ts
+++ tests/calculator.ts
@@ -1,16 +1,33 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { publicKey } from "@project-serum/anchor/dist/cjs/utils";
 import { Calculator } from "../target/types/calculator";
+//const { SystemProgram } = anchor.web3;
+import { expect, assert } from 'chai';
+
 
 describe("calculator", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
 
+  const provider = anchor.AnchorProvider.env();
   const program = anchor.workspace.Calculator as Program<Calculator>;
+  const datastore = anchor.web3.Keypair.generate()
+  const soyyo = anchor.Wallet.local();
+  anchor.setProvider(anchor.AnchorProvider.env());
+  const autoridad = provider.wallet.publicKey;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+
+  it("Creaste una calculadora", async () => {
+    const txSignature = await program.rpc.initialize(provider.wallet.publicKey, {
+      accounts: {
+        datastore: datastore.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [soyyo.payer],
+    });
+      
+      console.log("Your transaction signature", txSignature);
+    });
   });
-});
+
