4e7809148928e599275e56d80a50ad6eb2ed1cb5
Mateo Lafalce <mateolafalce3@gmail.com>
2022-07-17 19:58:58 -0300

    17/7/2022

--- programs/calculator/src/lib.rs
+++ programs/calculator/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("8UmDMaghFgLMnQXZXYnNJb2Uxpk1NUHMBo5ZbkeFYmwA");
+declare_id!("3E7U3DA7oLxDS4LN7fVpLfouuYka4jSuoq2a4pdLkVux");
 
 #[program]
 pub mod calculator {
@@ -8,36 +8,71 @@ pub mod calculator {
 
     pub fn initialize(
         ctx: Context<Initialize>, 
-        autoridad: Pubkey
+        autoridad: Pubkey,
+        add1: u64,add2: u64,
+        div1: u64,div2: u64,
+        sub1: u64,sub2: u64,
+        mul1: u64,mul2: u64,
+        squ:u64,
+        per: u64,
+        numtoper: u64
     ) -> Result<()> {
 
         let number = &mut ctx.accounts.datastore;
+        let clock = Clock::get().unwrap();
 
         number.autoridad = autoridad;
+        number.tiempo = clock.unix_timestamp;
 
-        number.add1 = 23;
-        number.add2 = 34;
-        number.division1 = 34;
-        number.division2 = 98;
+        number.add1 = add1;number.add2 = add2;
+        number.div1 = div1;number.div2 = div2;
+        number.sub1 = sub1;number.sub2 = sub2;
+        number.mul1 = mul1;number.mul2 = mul2;
+        number.squ = squ;number.per = per; 
+        number.numtoper = numtoper;
 
-        pub fn abc(num1: u64, num2: u64) {
-            let c: u64 = num1 + num2;
+        fn add(num1: u64, num2: u64) {
+            let c: f64 = (num1 + num2) as f64;
             msg!("The result of the sum is {}", c);
         }
-        pub fn def(num1: u64, num2: u64){
-            let c: u64 = num1 / num2;
+        fn div(num1: u64, num2: u64){
+            let c: f64 = (num1 / num2) as f64;
             msg!("The result of the division is {}", c);  
         }
+        fn sub(num1: u64, num2: u64){
+            let c: f64 = (num1 - num2) as f64;
+            msg!("The result of the subtraction is {}", c);  
+        }
+        fn mul(num1: u64, num2: u64){
+            let c: f64 = (num1 * num2) as f64;
+            msg!("The result of the multiplication is {}", c);  
+        }
+        fn squa(a: u64){
+            let c: f64 = (a * a) as f64;
+            msg!("The number squared is {}", c);
+        }
+        fn perc(a: u64, b: u64){
+            let hundred: u64 = 100;
+            let c: f64 = (b * (a / hundred)) as f64;
+            msg!("the percentage is {}", c);
+        }
 
-        abc(number.add1, number.add2);
-        def(number.division1, number.division2);
+        add(number.add1, number.add2);
+        div(number.div1, number.div2);
+        sub(number.sub1, number.sub2);
+        mul(number.mul1, number.mul2);
+        squa(number.squ);
+        perc(number.per, number.numtoper);
+
+        msg!("Autor: {}", number.autoridad);
+        msg!("Hora: {}", number.tiempo);
     Ok(())
 }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 30)]
+    #[account(init, payer = user, space = DataStore::LEN)]
     pub datastore: Account<'info, DataStore>,
     #[account(mut)]
     pub user: Signer<'info>,
@@ -46,9 +81,31 @@ pub struct Initialize<'info> {
 
 #[account]
 pub struct DataStore {
-    add1: u64,
-    add2: u64,
-    division1: u64,
-    division2: u64,
+    add1: u64,add2: u64,
+    div1: u64,div2: u64,
+    sub1: u64,sub2: u64,
+    mul1: u64,mul2: u64,
+    squ: u64,
+    per: u64, 
+    numtoper: u64,
+    tiempo: i64,
     autoridad: Pubkey
+}
+
+impl DataStore {
+    const LEN: usize = DISCRIMINATOR 
+    + PUBLIC_KEY 
+    + TIEMPO
+    + NUMBERS;
+}
+
+const DISCRIMINATOR: usize = 8;
+const PUBLIC_KEY: usize = 32;
+const NUMBERS: usize = 8 * 11;
+const TIEMPO: usize = 8;
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Your number is too big for a character u64")]
+    TooLong
 }
--- tests/calculator.ts
+++ tests/calculator.ts
@@ -2,32 +2,45 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { publicKey } from "@project-serum/anchor/dist/cjs/utils";
 import { Calculator } from "../target/types/calculator";
-//const { SystemProgram } = anchor.web3;
+const { SystemProgram } = anchor.web3;
 import { expect, assert } from 'chai';
 
 
 describe("calculator", () => {
-  // Configure the client to use the local cluster.
-
   const provider = anchor.AnchorProvider.env();
-  const program = anchor.workspace.Calculator as Program<Calculator>;
-  const datastore = anchor.web3.Keypair.generate()
-  const soyyo = anchor.Wallet.local();
-  anchor.setProvider(anchor.AnchorProvider.env());
-  const autoridad = provider.wallet.publicKey;
-
+  anchor.setProvider(provider);
+  const program = anchor.workspace.Calculator;
+  const datastore = anchor.web3.Keypair.generate();
 
   it("Creaste una calculadora", async () => {
-    const txSignature = await program.rpc.initialize(provider.wallet.publicKey, {
+
+    const txSignature = await program.rpc.initialize(
+      provider.wallet.publicKey, //Owner Key
+      add1 = new anchor.BN(2), //add1
+      add2 = new anchor.BN(45), //add2
+      new anchor.BN(78), //div1
+      new anchor.BN(9), //div2
+      new anchor.BN(75), //sub1
+      new anchor.BN(36), //sub2
+      new anchor.BN(49), //mul1
+      new anchor.BN(28), //mul2
+      new anchor.BN(2), //square number
+      new anchor.BN(9), //percentage
+      new anchor.BN(123), //number to percentage
+    {
       accounts: {
         datastore: datastore.publicKey,
         user: provider.wallet.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
+        systemProgram: SystemProgram.programId,
       },
-      signers: [soyyo.payer],
+      signers: [datastore],
     });
       
       console.log("Your transaction signature", txSignature);
+      //assert.equal(txSignature.autoridad.toBase58(), program.provider.wallet.publicKey.toBase58());
+      //assert.equal(txSignature.add1, anchor.BN(2));
+      //assert.equal(txSignature.add2, anchor.BN(45));
+      //assert.ok(txSignature.timestamp);
     });
   });
 
